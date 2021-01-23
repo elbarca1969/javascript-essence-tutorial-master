1ab8b11a7eebf346e3379e0d08ec4b660a6ce2d5
elbarca1969 <72918547+elbarca1969@users.noreply.github.com>
2021-01-23 16:43:31 +0900

    修了確認＿スコープ

--- 030_スコープ/910_修了確認_スコープ/start/main.js
+++ 030_スコープ/910_修了確認_スコープ/start/main.js
@@ -9,8 +9,8 @@
 function fn() {
     if(true) {
         let a = 'fn called';
+        return a; // ReferenceError: a is not defined
     }
-    return a; // ReferenceError: a is not defined
 }
 
 const result = fn();
@@ -26,7 +26,7 @@ function fn2() {
     console.log(val); // 期待値->'val1'
 
     if(true) {
-        var val = 'val2';
+        let val = 'val2';
         console.log(val); // 期待値->'val2'
     }
 
@@ -46,3 +46,21 @@ fn2();
  * increment(); // 期待値->4
  */
 
+function incrementFactory() {
+
+    let num = 0;
+
+    function increment() {
+        num = num + 1;
+        console.log(num);
+    }
+
+    return increment
+}
+
+const increment = incrementFactory();
+
+increment();
+increment();
+increment();
+increment();
